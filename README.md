87051cac408c26c5cdd3c26f79e491fb38753aea
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-03-30 03:38:34 +0900

    modified heading, adding README

---
+++ README.md
@@ -0,0 +1,26 @@
+# BLOG CRUD APP
+
+[Blog Image]('./assets/images/blog_all.png)
+
+## Description
+
+I made this blog crud app to practice and learn MERN stack
+
+## Technologies
+
+- React
+- Node.js
+- MongoDB Atlas
+- CSS3
+
+## Features
+
+- Posting blogs
+- Editing blogs
+- Deleting blogs
+- Adding comments
+- Like button and dislike button
+
+[Posting log Image]('./assets/images/blog_post.png)
+[Posting log Image]('./assets/images/blog_read.png)
+[Posting log Image]('./assets/images/blog_edit.png)
--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,133 +1,129 @@
-const Blog = require('../models/blog.model')
-const Comment = require('../models/comment.model')
+const Blog = require("../models/blog.model");
+const Comment = require("../models/comment.model");
 
 const getById = (blogId) => {
   return Blog.findById(blogId, (err, data) => {
-      if(err) console.log(err)
-      return data
-  }).clone()
-}
+    if (err) console.log(err);
+    return data;
+  }).clone();
+};
 
-
-exports.getAllBlogs = async(req, res, next) => {
+exports.getAllBlogs = async (req, res, next) => {
   // console.log(req)
   Blog.find((err, blogs) => {
-    if (err) console.log(err)
-    res.render('index', {
-      pageTitle: 'All Blogs',
+    if (err) console.log(err);
+    res.render("index", {
+      pageTitle: "All Blogs",
       blogs: blogs,
-    })
-  })
-}
+    });
+  });
+};
 
 exports.getAddBlogs = (req, res, next) => {
-  res.render('add-edit-blogs', {
-    pageTitle: 'Post Blogs',
+  res.render("add-edit-blogs", {
+    pageTitle: "Post a blog",
     editing: false,
-  })
-}
+  });
+};
 
-exports.postAddBlogs = async(req, res, next) => {
-  const { 
+exports.postAddBlogs = async (req, res, next) => {
+  const {
     title,
-    // imageUrl, 
-    content, 
+    // imageUrl,
+    content,
     // createdDate,
     like,
-    dislike
-  } = req.body
+    dislike,
+  } = req.body;
 
   const blog = new Blog({
-    title, 
-    // imageUrl, 
-    content, 
+    title,
+    // imageUrl,
+    content,
     // createdDate,
     blogId: req.blog,
     like: 0,
-    dislike: 0
-  })
-  await blog.save()
-  res.redirect('/')
-}
+    dislike: 0,
+  });
+  await blog.save();
+  res.redirect("/");
+};
 
 exports.getBlogById = async (req, res, next) => {
-  
   const {
     params: { blogId },
-  } = req
+  } = req;
   // const commentsData = {}
-  
-  const blog = await getById(blogId)
-  const comments = await getById(blogId)
-  
-  Comment.find({ blogId: blogId}, (err, comment)=>{
-    res.render('blog-detail', {
+
+  const blog = await getById(blogId);
+  const comments = await getById(blogId);
+
+  Comment.find({ blogId: blogId }, (err, comment) => {
+    res.render("blog-detail", {
       pageTitle: blog.title,
       blog: blog,
-      comments: comment
-    })
-    
-  })
+      comments: comment,
+    });
+  });
+};
 
-}
-
-exports.getEditBlog = async(req, res, next) => {
+exports.getEditBlog = async (req, res, next) => {
   // const editMode = req.query.edit
   // console.log(req.query)
   // if (!editMode) res.redirect('/')
 
-  const { blogId } = req.params
-  const blog = await getById(blogId)
-  
-  res.render('add-edit-blogs', {
-    pageTitle: 'Edit blog',
+  const { blogId } = req.params;
+  const blog = await getById(blogId);
+
+  res.render("add-edit-blogs", {
+    pageTitle: "Edit blog",
     editing: true,
     blog: blog,
-  })
-}
+  });
+};
 
-exports.postEditBlog = async(req, res, next) => {
-  const { 
-      blogId, 
-      title, 
-      // imageUrl, 
-      content, 
-    } = req.body
+exports.postEditBlog = async (req, res, next) => {
+  const {
+    blogId,
+    title,
+    // imageUrl,
+    content,
+  } = req.body;
 
-    const blog = await getById(blogId)
-  
-    blog.title = title
-    // blog.imageUrl = imageUrl
-    blog.content = content
-  
-    await blog.save()
-    res.redirect('/')
-}
+  const blog = await getById(blogId);
 
-exports.postDeleteBlog= (req, res, next) => {
-  const blogId = req.body.blogId
-  console.log('delete : ', blogId)
+  blog.title = title;
+  // blog.imageUrl = imageUrl
+  blog.content = content;
+
+  await blog.save();
+  res.redirect("/");
+};
+
+exports.postDeleteBlog = (req, res, next) => {
+  const blogId = req.body.blogId;
+  console.log("delete : ", blogId);
 
   Blog.findByIdAndRemove(blogId, (err) => {
-    if(err) console.log('error')
+    if (err) console.log("error");
   });
-  res.redirect('/')
-}
+  res.redirect("/");
+};
 
-exports.postCountLike = async(req, res, next) => {
-  const { blogId } = req.body 
-  const blog = await getById(blogId)
-  blog.like++
+exports.postCountLike = async (req, res, next) => {
+  const { blogId } = req.body;
+  const blog = await getById(blogId);
+  blog.like++;
 
-  await blog.save()
-  res.redirect(`/blogs/${blogId}`)
-}
+  await blog.save();
+  res.redirect(`/blogs/${blogId}`);
+};
 
-exports.postCountDislike = async(req, res, next) => {
-  const { blogId } = req.body 
-  const blog = await getById(blogId)
-  blog.dislike++
+exports.postCountDislike = async (req, res, next) => {
+  const { blogId } = req.body;
+  const blog = await getById(blogId);
+  blog.dislike++;
 
-  await blog.save()
-  res.redirect(`/blogs/${blogId}`)
-}
+  await blog.save();
+  res.redirect(`/blogs/${blogId}`);
+};
